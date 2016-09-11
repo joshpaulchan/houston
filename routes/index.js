de736ba8e3c34414da5e91614cf105432ad3388e
joshpaulchan <joshpaulchan@gmail.com>
2016-09-10 23:54:35 -0400

    Remove render

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var router = require('express').Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
 
 module.exports = router;
